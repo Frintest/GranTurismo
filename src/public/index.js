3f8fd293d690cfd550ae0bf8ba91d5f8d6d54f42
Dmitry <dimaruzaev2@gmai.com>
2022-02-10 17:39:05 +0300

    code r

--- src/public/index.js
+++ src/public/index.js
@@ -3,4 +3,11 @@ import './scss/main.scss';
 import './js/app.js';
 
 // assets
-import './models/auto.glb';
+import './models/auto2.glb';
+import './models/wheel.glb';
+import './textures/skybox/skybox_nx.jpg';
+import './textures/skybox/skybox_ny.jpg';
+import './textures/skybox/skybox_nz.jpg';
+import './textures/skybox/skybox_px.jpg';
+import './textures/skybox/skybox_py.jpg';
+import './textures/skybox/skybox_pz.jpg';
--- src/public/js/app.js
+++ src/public/js/app.js
@@ -10,7 +10,7 @@ class BaseScene {
 		// scene
 		const scene = new BABYLON.Scene(this.engine);
 		scene.clearColor = new BABYLON.Color3.FromHexString('#94bed0')
-		scene.debugLayer.show();
+		// scene.debugLayer.show();
 
 		// camera
 		const camera = new BABYLON.ArcRotateCamera('camera', 0, Math.PI / 2.5, 60, new BABYLON.Vector3(0, 0, 0), scene);
@@ -18,17 +18,23 @@ class BaseScene {
 		scene.activeCamera.attachControl(canvas, true);
 
 		// light
-		const light = new BABYLON.DirectionalLight('light', new BABYLON.Vector3(-200, -100, 30), scene);
+		const light = new BABYLON.DirectionalLight('light', new BABYLON.Vector3(200, -100, 70), scene);
+		light.diFuse = new BABYLON.Color3.FromHexString('#f9f9dc');
+		light.specular = new BABYLON.Color3.FromHexString('#f9f9dc');
 
 		const lightCamera = new BABYLON.HemisphericLight('lightCamera', new BABYLON.Vector3(camera.position.x, camera.position.y, camera.position.z), scene);
+
+		// skybox
+		const skybox = new BABYLON.CubeTexture('./textures/skybox', scene);
+		scene.createDefaultSkybox(skybox, true, 1000);
 
 
 		// Road
 		class Road {
 			constructor() {
 				this.roadMesh = BABYLON.MeshBuilder.CreateBox('roadMesh', {
-					width: 65,
-					depth: 40,
+					width: 50,
+					depth: 1000,
 					height: 1.4
 				}, scene);
 
@@ -44,12 +50,12 @@ class BaseScene {
 		// Auto
 		class Auto {
 			constructor() {
-				BABYLON.SceneLoader.Append('./models/', 'auto.glb', scene, (result) => {
-					const mesh = result.meshes[1];
-					mesh.name = 'autoMesh';
+				BABYLON.SceneLoader.Append('./models/', 'auto2.glb', scene, (result) => {
+					this.mesh = result.meshes[6];
+					this.mesh.name = 'autoMesh';
 
-					mesh.scaling = new BABYLON.Vector3(5, 5, 5);
-					mesh.position.y = 1.27;
+					this.mesh.scaling = new BABYLON.Vector3(6, 6, 6);
+					this.mesh.position.y = 1;
 				});
 			}
 		}
@@ -57,15 +63,110 @@ class BaseScene {
 		new Auto();
 
 
+		// Wheel
+		class Wheel {
+			constructor() {
+				BABYLON.SceneLoader.Append('./models/', 'wheel.glb', scene, (result) => {
+					// wheelFrontLeft
+					this.wheelFrontLeft = result.meshes[2];
+					this.wheelFrontLeft.name = 'wheelFrontLeft';
+					this.wheelFrontLeft.position = new BABYLON.Vector3(5.1, 2.1, -9.4);
+					this.wheelFrontLeft.scaling = new BABYLON.Vector3(1.82, 1.82, 1.82);
+
+					// wheelFrontRight
+					this.wheelFrontRight = this.wheelFrontLeft.clone('wheelFrontRight');
+					this.wheelFrontRight.position = new BABYLON.Vector3(-5.55, this.wheelFrontRight.position.y, this.wheelFrontRight.position.z);
+
+					// wheelRearRight
+					this.wheelRearRight = this.wheelFrontRight.clone('wheelRearRight');
+					this.wheelRearRight.position = new BABYLON.Vector3(this.wheelRearRight.position.x, this.wheelRearRight.position.y, 8.07);
+
+					// wheelRearLeft
+					this.wheelRearLeft = this.wheelFrontLeft.clone('wheelRearLeft');
+					this.wheelRearLeft.position = new BABYLON.Vector3(this.wheelRearLeft.position.x, this.wheelRearLeft.position.y, 8.07);
+				});
+			}
+		}
+
+		new Wheel();
+
+
 		// GlowLayer
 		class GlowLayer {
 			constructor() {
 				this.glowLayer = new BABYLON.GlowLayer('glowLayer', scene);
-				this.glowLayer.intensity = 2;
+				this.glowLayer.intensity = 5;
 			}
 		}
 
 		new GlowLayer();
+
+
+		// PhysicsObjects
+		class PhysicsObjects {
+			constructor() {
+				scene.enablePhysics(new BABYLON.Vector3(0, -9.81, 0), new BABYLON.AmmoJSPlugin());
+
+				this.processingPhysics();
+			}
+
+			processingPhysics() {
+				scene.getMeshByName('roadMesh').physicsImpostor = new BABYLON.PhysicsImpostor(scene.getMeshByName('roadMesh'), BABYLON.PhysicsImpostor.BoxImpostor, {
+					mass: 0,
+					restitution: 0,
+					friction: 100
+				});
+
+
+				this.motorMesh = BABYLON.MeshBuilder.CreateBox('motorMesh', {
+					width: 10,
+					depth: 15,
+					height: 1
+				}, scene);
+				this.motorMesh.position.y = 3;
+
+				this.motorMesh.physicsImpostor = new BABYLON.PhysicsImpostor(this.motorMesh, BABYLON.PhysicsImpostor.BoxImpostor, {
+					mass: 1500,
+					restitution: 0,
+					friction: 100
+				});
+
+				scene.getMeshByName('autoMesh').parent = this.motorMesh;
+
+
+				const wheels = [scene.getMeshByName('wheelFrontLeft'), scene.getMeshByName('wheelFrontRight'), scene.getMeshByName('wheelRearLeft'), scene.getMeshByName('wheelRearRight')];
+
+				const addPhysicsWheels = (arrWheels) => {
+					arrWheels.forEach((wheel) => {
+						wheel.physicsImpostor = new BABYLON.PhysicsImpostor(wheel, BABYLON.PhysicsImpostor.MeshImpostor, {
+							mass: 150,
+							restitution: 0,
+							friction: 100
+						});
+					});
+				}
+
+				addPhysicsWheels(wheels);
+
+				const jointWheels = (meshArr) => {
+					meshArr.forEach((mesh) => {
+						mesh.rotation = new BABYLON.Vector3(0, 0, 0);
+						const joint = new BABYLON.HingeJoint({
+							mainPivot: new BABYLON.Vector3(mesh.position.x, mesh.position.y - 2, mesh.position.z),
+							mainAxis: new BABYLON.Vector3(1, 0, 0)
+						});
+						this.motorMesh.physicsImpostor.addJoint(mesh.physicsImpostor, joint);
+						joint.setMotor(30, 230);
+					});
+				};
+
+				jointWheels(wheels);
+			}
+		}
+
+		setTimeout(() => {
+			// new PhysicsObjects();
+		}, 2000);
 
 
 		// GenerateShadowsMap
@@ -138,7 +239,7 @@ class BaseScene {
 			const meshesReceiveShadows = [scene.getMeshByName('roadMesh')]; // объеты отображающие тень
 
 			new GenerateShadowsMap(meshesGetShadowMap, meshesReceiveShadows);
-		}, 1000);
+		}, 3000);
 
 
 		// render
